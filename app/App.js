74580e2ea73fb32b8a350a641e0f3dc5cf73389c
Guido Perdomo <guidop91@gmail.com>
2020-02-04 09:18:05 -0400

    Create navlink markup

--- app/App.js
+++ app/App.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Popular from './components/Popular'
 
 export default class App extends React.Component {
   render() {
-    const auth = true;
-    if (auth) {
-      return <h1>Welcome back!</h1>
-    }
     return (
-      <div><h1>Log In to see your dashboard</h1></div>
-    );
+      <div className='container'>
+        <Popular />
+      </div>
+    )
   }
 }
