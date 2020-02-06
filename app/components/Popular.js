22a252ec326ca2dc5110c797f63bfa5fd3902bb4
Guido Perdomo <guidop91@gmail.com>
2020-02-06 09:11:52 -0400

    State in components

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,18 +1,46 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedLanguage: 'All'
+    }
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
   render () {
-    const languages = ['Javascript', 'Python', 'Java', 'PHP', 'C'];
     return (
-      <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={language}>
-            <button className='btn-clear nav-link'>
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <LanguagesNav
+        updateLanguage={this.updateLanguage}
+        selectedLanguage={this.state.selectedLanguage} 
+      />
     )
   }
+}
+
+function LanguagesNav (props) {
+  const { updateLanguage, selectedLanguage } = props;
+  const languages = ['All', 'Javascript', 'Python', 'Java', 'PHP', 'C'];
+  const btnClass = (language) => selectedLanguage === language
+    ? 'btn-clear nav-link highlighted'
+    : 'btn-clear nav-link';
+  return (
+    <ul className='flex-center'>
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className={btnClass(language)}
+            onClick={() => updateLanguage(language)}>
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
 }
