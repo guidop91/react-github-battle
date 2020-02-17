58443238e27387e38fd3392d873d21e8c293b3b1
Guido Perdomo <guidop91@gmail.com>
2020-02-17 09:12:54 -0400

    Add player battle component

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Popular from './components/Popular'
+import Popular from './components/Popular';
+import Battle from './components/Battle';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Popular />
+        <Battle />
       </div>
     )
   }
