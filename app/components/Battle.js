0383610296bd0f4c36a0d803db585c1a92958e1d
Guido Perdomo <guidop91@gmail.com>
2020-02-18 09:17:04 -0400

    Player battle rendering user

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleSubmit(id, playerName) {
+    this.setState({
+      [id]: playerName
+    })
+  }
+
+  handleReset(id) {
+    this.setState({
+      [id]: null
+    })
+  }
+
   render () {
+    const { playerOne, playerTwo } = this.state;
     return (
       <React.Fragment>
         <Instructions />
+        <div>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {playerOne === null ? (
+              <PlayerInput
+                label='Player One'
+                onSubmit={(player) => this.handleSubmit('playerOne', player)}
+              />
+            ) : (
+              <PlayerPreview 
+                username={playerOne}
+                label='Player One'
+                onReset={() => this.handleReset('playerOne')}
+              />
+            )}
+
+            {playerTwo === null ? (
+              <PlayerInput
+                label='Player Two'
+                onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+              />
+            ) : (
+              <PlayerPreview 
+                username={playerTwo}
+                label='Player Two'
+                onReset={() => this.handleReset('playerTwo')}
+              />
+            )}
+          </div>
+        </div>
       </React.Fragment>
     )
   }
@@ -65,7 +118,36 @@ class PlayerInput extends React.Component {
 PlayerInput.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired
+};
+
+function PlayerPreview ({ username, onReset, label }) {
+  return (
+    <div className='column player'>
+      <h3 className='player-label'>{label}</h3>
+      <div className='row bg-light'>
+        <div className='player-info'>
+          <img 
+            className='avatar-small'
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className='link'>
+            {username}
+          </a>
+        </div>
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+        </button>
+      </div>
+    </div>
+  )
 }
+
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
 
 function Instructions () {
   return (
