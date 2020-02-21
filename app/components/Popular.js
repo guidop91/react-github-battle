b2020c8ee960b784ebbdcc5367e6b62a2abd1687
Guido Perdomo <guidop91@gmail.com>
2020-02-21 09:03:58 -0400

    Abstract into Card component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle, FaCode } from 'react-icons/fa';
+import Card from './Card';
 
 export default class Popular extends React.Component {
   constructor(props) {
@@ -98,38 +99,34 @@ function ReposGrid ({ repos }) {
         const { name, owner, html_url, stargazers_count, forks, open_issues } = repo;
         const { login, avatar_url } = owner;
         return (
-          <li key={html_url} className='card bg-light'>
-            <h4 className='header-lg center-text'>
-              #{index + 1}
-            </h4>
-            <img 
-              className='avatar'
-              src={avatar_url}
-              alt={`Avatar for ${login}`}
-            />
-            <h2 className='center-text'>
-              <a className='link' href={html_url}>{login}</a>
-            </h2>
-            <ul className='card-list'>
-              <li>
-                <FaUser color='rgb(255, 191, 116' size={22} />
-                <a href={`https://github.com/${login}`}>
-                  {login}
-                </a>
-              </li>
-              <li>
-                <FaStar color='rgb(255, 215, 0)' size={22} />
-                {stargazers_count.toLocaleString()} stars
-              </li>
-              <li>
-                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
-                {forks.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
-                {open_issues.toLocaleString()} open issues
-              </li>
-            </ul>
+          <li key={html_url}>
+            <Card
+              avatar={avatar_url}
+              header={`#${index + 1}`}
+              href={html_url}
+              name={login}
+            >
+              <ul className='card-list'>
+                <li>
+                  <FaUser color='rgb(255, 191, 116' size={22} />
+                  <a href={`https://github.com/${login}`}>
+                    {login}
+                  </a>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  {open_issues.toLocaleString()} open issues
+                </li>
+              </ul>
+            </Card>
           </li>
         )
       })}
