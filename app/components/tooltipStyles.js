92ac9d17716dc78697642b3353b332f6b97c57f4
Guido Perdomo <guidop91@gmail.com>
2020-02-26 09:32:19 -0400

    Add tooltip on hover

---
+++ app/components/tooltipStyles.js
@@ -0,0 +1,23 @@
+const styles = {
+  container: {
+    position: 'relative',
+    display: 'flex',
+  },
+  tooltip: {
+    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+    borderRadius: '3px',
+    bottom: '100%',
+    boxSizing: 'border-box',
+    color: '#FFFFFF',
+    fontSize: '14px',
+    left: '50%',
+    marginBottom: '5px',
+    marginLeft: '-80px',
+    padding: '7px',
+    position: 'absolute',
+    textAlign: 'center',
+    width: '160px'
+  }
+}
+
+export default styles;
