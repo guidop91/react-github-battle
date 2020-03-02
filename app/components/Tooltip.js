9532e918b22ccb215bd7558733eb02a3b241a10b
Guido Perdomo <guidop91@gmail.com>
2020-03-02 09:36:28 -0400

    Add HOC for hovering

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,46 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './tooltipStyles';
+import withHover from './withHover';
 
-export default class Tooltip extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hovering: false
-    }
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
-
-  mouseOver() {
-    this.setState({
-      hovering: true
-    })
-  }
-
-  mouseOut() {
-    this.setState({
-      hovering: false
-    })
-  }
-  render() {
-    const { text, children } = this.props;
-    const { hovering } = this.state;
-
-    return (
-      <div
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-        style={styles.container}
-      >
-        {hovering === true && <div style={styles.tooltip}>{text}</div>}
-        {children}
-      </div>
-    )
-  }
+function Tooltip({ text, children, hovering }) {
+  return (
+    <div style={styles.container}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  )
 }
 
 Tooltip.propTypes = {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  hovering: PropTypes.bool.isRequired
 };
+
+export default withHover(Tooltip);
