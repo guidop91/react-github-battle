8da75a599995a027bb615546be5b9008daa70722
Guido Perdomo <guidop91@gmail.com>
2020-03-12 08:55:52 -0400

    More styles for their dark theme lovers

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import Results from './Results';
+import { ThemeConsumer } from '../context/theme';
 
 export default class Battle extends React.Component {
   constructor(props) {
@@ -112,29 +113,34 @@ class PlayerInput extends React.Component {
 
   render() {
     return (
-      <form className='column player' onSubmit={this.handleSubmit}>
-        <label htmlFor='username' className='player-label'>
-          {this.props.label}
-        </label>
-        <div className='row player-inputs'>
-          <input
-            autoComplete='off'
-            className='input-light'
-            id='username'
-            onChange={this.handleChange}
-            placeholder='github username'
-            type='text'
-            value={this.state.username}
-          />
-          <button
-            className='btn dark-btn'
-            type='submit'
-            disabled={!this.state.username}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+      <ThemeConsumer>
+        {({ theme }) => (
+          <form className='column player' onSubmit={this.handleSubmit}>
+            <label htmlFor='username' className='player-label'>
+              {this.props.label}
+            </label>
+            <div className='row player-inputs'>
+              <input
+                autoComplete='off'
+                className={`input-${theme}`}
+                id='username'
+                onChange={this.handleChange}
+                placeholder='github username'
+                type='text'
+                value={this.state.username}
+              />
+              <button
+                className={`btn ${theme === 'dark' ? 'light' : 'dark'}-btn`}
+                type='submit'
+                disabled={!this.state.username}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+
+        )}
+      </ThemeConsumer>
     )
   }
 }
@@ -146,24 +152,29 @@ PlayerInput.propTypes = {
 
 function PlayerPreview ({ username, onReset, label }) {
   return (
-    <div className='column player'>
-      <h3 className='player-label'>{label}</h3>
-      <div className='row bg-light'>
-        <div className='player-info'>
-          <img 
-            className='avatar-small'
-            src={`https://github.com/${username}.png?size=200`}
-            alt={`Avatar for ${username}`}
-          />
-          <a href={`https://github.com/${username}`} className='link'>
-            {username}
-          </a>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className='column player'>
+          <h3 className='player-label'>{label}</h3>
+          <div className={`row bg-${theme}`}>
+            <div className='player-info'>
+              <img 
+                className='avatar-small'
+                src={`https://github.com/${username}.png?size=200`}
+                alt={`Avatar for ${username}`}
+              />
+              <a href={`https://github.com/${username}`} className='link'>
+                {username}
+              </a>
+            </div>
+            <button className='btn-clear flex-center' onClick={onReset}>
+              <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+            </button>
+          </div>
         </div>
-        <button className='btn-clear flex-center' onClick={onReset}>
-          <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
-        </button>
-      </div>
-    </div>
+
+      )}
+    </ThemeConsumer>
   )
 }
 
@@ -175,24 +186,28 @@ PlayerPreview.propTypes = {
 
 function Instructions () {
   return (
-    <div className='instructions-container'>
-      <h1 className='center-text header-lg'>
-        Instructions
-      </h1>
-      <ol className='container-sm grid center-text battle-instructions'>
-        <li>
-          <h3 className='header-sm'>Enter two Github users</h3>
-          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
-        </li>
-        <li>
-          <h3 className='header-sm'>Battle</h3>
-          <FaFighterJet className='bg-light' color='#727272' size={140} />
-        </li>
-        <li>
-          <h3 className='header-sm'>See the winners</h3>
-          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
-        </li>
-      </ol>
-    </div>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className='instructions-container'>
+          <h1 className='center-text header-lg'>
+            Instructions
+          </h1>
+          <ol className='container-sm grid center-text battle-instructions'>
+            <li>
+              <h3 className='header-sm'>Enter two Github users</h3>
+              <FaUserFriends className={`bg-${theme}`} color='rgb(255, 191, 116)' size={140} />
+            </li>
+            <li>
+              <h3 className='header-sm'>Battle</h3>
+              <FaFighterJet className={`bg-${theme}`} color='#727272' size={140} />
+            </li>
+            <li>
+              <h3 className='header-sm'>See the winners</h3>
+              <FaTrophy className={`bg-${theme}`} color='rgb(255, 215, 0)' size={140} />
+            </li>
+          </ol>
+        </div>
+      )}
+    </ThemeConsumer>
   )
 }
